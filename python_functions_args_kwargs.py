148620ceadb2c2f61738c2f22e7659512e533109
xanthium-enterprises <contact@xanthium.in>
2024-02-03 13:27:02 +0530

    args and kwargs

--- python_functions_args_kwargs.py
+++ python_functions_args_kwargs.py
@@ -3,5 +3,22 @@
 def function_1(*args):
     print(args)
     
-    
-function_1(1,2,3,4,)
+def function_2(**kwargs):
+    print(kwargs)
+
+
+def function_3(*args,**kwargs):
+    print(args)
+    print(kwargs)
+'''    
+function_1(1,2,3,4)
+function_2(name ='john',age =28)'''
+
+#function_3(1,2,3,'a','b','c',name ='john',age =28)
+
+a = [1,2,3,'a','b','c']
+kw ={'name' :'john','age' :28 }
+
+function_3(a,kw)
+function_3(*a,**kw)
+
