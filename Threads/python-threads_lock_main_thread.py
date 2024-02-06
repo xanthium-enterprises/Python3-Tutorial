bf3dc29f57fce30197203f077a21f704d36053ac
xanthium-enterprises <contact@xanthium.in>
2024-02-06 18:27:40 +0530

    Update python-threads_lock_main_thread.py

--- Threads/python-threads_lock_main_thread.py
+++ Threads/python-threads_lock_main_thread.py
@@ -3,11 +3,10 @@ import threading
 
 
 def function_t(): 
-    lock.acquire()
     
-    print('\nfunction_t entered')
-    time.sleep(5)
-    print('function_t exited')
+    tl = lock.acquire(timeout = 10 )
+    print(tl)
+    
     
     lock.release()
     
@@ -16,16 +15,16 @@ lock = threading.Lock() #create the lock object
 
 t1 = threading.Thread(target = function_t)
 
-print( 'locking main thread for  seconds')
-lock.acquire() #acquire the lock in main thread and never release preventing  
+print( 'locking main thread forever')
+l= lock.acquire() #acquire the lock in main thread and never release preventing  
                # t1 from ever running
 t1.start()
-time.sleep(10)  # or release after 5 seconds             
-#lock.release()
-print( 'lock released after 5 seconds')
+
+print( 'lock status =',l)
 
 
 t1.join()
+
 
 print('End of Main Thread')
 
